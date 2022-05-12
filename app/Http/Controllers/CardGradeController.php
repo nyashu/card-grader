661b7a031111a7d132485748f881216c53adb36e
Yashu Neupane <neupaneyashu19@gmail.com>
2022-05-12 19:31:08 +0545

    API response implemented

--- app/Http/Controllers/CardGradeController.php
+++ app/Http/Controllers/CardGradeController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Customer;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class CardGradeController extends Controller
 {
@@ -33,7 +35,24 @@ class CardGradeController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate([
+            'name' => 'required',
+            'email' => 'required',
+            'comment' => 'nullable',
+            'image' => 'required'
+        ]);
+
+        $customer = Customer::create($validated);
+
+        $data = Http::post("https://x65k5b8f46.execute-api.us-east-1.amazonaws.com/RestV1");
+
+        json_decode($data);
+        $data = $data['body'][11];
+
+        if ($customer)
+            return view('frontend.grade', compact('data'));
+
+        return back()->with('message', 'Error occured, Try again later !!');
     }
 
     /**
--- resources/views/frontend/grade.blade.php
+++ resources/views/frontend/grade.blade.php
@@ -6,17 +6,35 @@
             <div class="grade-text">
                 <h1 class="text-center">Welcome</h1>
                 <h2 class="text-center">Begin your journey to grade cards.</h2>
+
+
+                @if (session()->has('message'))
+                    <p class="alert alert-success text-center">{{ session()->get('message') }}</p>
+                @endif
+
+
+                @if ($errors->any())
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+
                 <div class="row">
                     <div class="col-md-6">
-                        <form>
+                        <form action="{{ route('grade-card.store') }}" method="POST">
+                            @csrf
                             <div class="form-group">
                                 <label for="exampleInputFullname1">Full Name</label>
-                                <input type="text" class="form-control" id="exampleInputFullname1"
+                                <input type="text" name="name" class="form-control" id="exampleInputFullname1"
                                     placeholder="Full name">
                             </div>
                             <div class="form-group">
                                 <label for="exampleInputEmail1">Email address</label>
-                                <input type="email" class="form-control" id="exampleInputEmail1"
+                                <input type="email" name="email" class="form-control" id="exampleInputEmail1"
                                     aria-describedby="emailHelp" placeholder="Enter email">
                                 <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone
                                     else.</small>
@@ -24,19 +42,32 @@
 
                             <div class="form-group">
                                 <label for="exampleFormControlTextarea1">Comments (Optional)</label>
-                                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-                              </div>
+                                <textarea class="form-control" name="comment" id="exampleFormControlTextarea1" rows="3"></textarea>
+                            </div>
 
-                                <div class="form-group">
-                                  <label for="exampleFormControlFile1">Upload your picture</label>
-                                  <input type="file" class="form-control-file" id="exampleFormControlFile1">
-                                </div>
+                            <div class="form-group">
+                                <label for="exampleFormControlFile1">Upload your picture</label>
+                                <input type="file" name="image" class="form-control-file form-control"
+                                    id="exampleFormControlFile1">
+                            </div>
                             <button type="submit" class="btn btn-danger submit-btn-grade">Submit</button>
                         </form>
                     </div>
 
-                    <div class="col-md-6">
-                        <div class="image-poke"> </div>
+                    <div class="col-md-6 result-parent">
+                        {{-- <a href="https://cf.bstatic.com/xdata/images/hotel/square200/39194987.webp?k=797ae9038baac981eac4d2deb95e8bc5abcf15097dc3fbd4ef9fd540425f6e4b&o=&s=1" download class="download-button">
+                            <i class="fa fa-download download-icon" aria-hidden="true"></i>
+                            <span class="download-text">download<span>
+                        </a> --}}
+                        @if (isset($data))
+                        
+                            <div class="alert alert-success result-alert" role="alert"><span class="text-danger">{{ $data > 5 ? "Well done !" : "Opps !" }}</span> 
+                                <br> <br> <h1>You have got <span class="text-danger">{{ $data }}</span> grade rating.</h1>
+                                <br><img src="{{ $data > 5 ? asset('frontend/img/celebrate.png') : asset('frontend/img/opps.png') }}" alt="">
+                            </div>
+                        @else
+                        <div class="image-poke"></div>
+                        @endif
                     </div>
                 </div>
 
